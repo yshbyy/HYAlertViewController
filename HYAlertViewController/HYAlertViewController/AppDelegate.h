83c153170b13ae230593f1afa177b586a0ebcc35
yshbyy <401973712@qq.com>
2016-07-01 16:01:13 +0800

    新建示例项目

---
+++ HYAlertViewController/HYAlertViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYAlertViewController
+//
+//  Created by yshbyy on 16/7/1.
+//  Copyright © 2016年 yshbyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYAlertViewController/HYAlertViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYAlertViewController
+//
+//  Created by yshbyy on 16/7/1.
+//  Copyright © 2016年 yshbyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
